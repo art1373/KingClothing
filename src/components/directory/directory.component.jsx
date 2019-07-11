f073ff2655ed6850a9607669871ccc970713d9e6
alireza <alirezatk73@gmail.com>
2019-07-11 14:52:50 +0430

    HomePage UI implementation

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, size, id }) => (
+          <MenuItem key={id} imageUrl={imageUrl} title={title} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
