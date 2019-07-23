8e27eeeea3effe3ee387ea1edd974b93e287c9a2
Alireza <Alirezatk73@gmail.com>
2019-07-23 10:25:56 +0430

    some fixes

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,7 +3,6 @@ import "./sign-up.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom.button.component";
 import {  auth, createUserProfileDocument } from "../../firebase/firebase.utils";
-import { async } from "q";
 
 class SignUp extends React.Component {
   constructor() {
