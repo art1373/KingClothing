a1d1849a4e3223c101e9c6e404541c13b598aa70
art1373 <alirezatk73@gmail.com>
2019-08-08 23:09:27 +0430

    added swal instead of alert

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
+import Swal from "sweetalert2";
 
-import StripeCheckout from 'react-stripe-checkout';
+import StripeCheckout from "react-stripe-checkout";
 
-const StripeCheckoutButton = ({price}) =>{
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_QdwbCwJOzw56vSwuGFOi100W00DIj30Q9P'
-const onToken = token =>{
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_QdwbCwJOzw56vSwuGFOi100W00DIj30Q9P";
+  const onToken = token => {
     console.log(token);
-    alert('Payment Succesful')
-}
+    Swal.fire("Payment Succesful");
+  };
 
-    return(
-        <StripeCheckout 
-            lable = 'Pay Now'
-            name='KingClothing co.Ltd'
-            billingAddress
-            shippingAddress
-            image="https://www.mykingandbay.com/images/1173/new/css/king-and-bay-logo-icon.svg"
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    );
-}
+  return (
+    <StripeCheckout
+      lable="Pay Now"
+      name="KingClothing co.Ltd"
+      billingAddress
+      shippingAddress
+      image="https://www.mykingandbay.com/images/1173/new/css/king-and-bay-logo-icon.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
 export default StripeCheckoutButton;
