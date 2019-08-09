3b573a81163e9c752e4870db1bab77f933045287
art1373 <alirezatk73@gmail.com>
2019-08-09 15:38:21 +0430

    syntax correction

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,12 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-
-
-import {
-  selectCartItems,
-  selectCartTotal
-} from '../../redux/cart/cart.selectors';
+import {  selectCartItems,  selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
