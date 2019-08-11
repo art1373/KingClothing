2e9df52db603cb688e24fdf2b0f5e4517f8d0b3b
art1373 <alirezatk73@gmail.com>
2019-08-11 18:18:40 +0430

    path correction

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { createStructuredSelector } from 'reselect';
 import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/Shop/shop.component';
+import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
